2967a734fcee38515747385da096dac1b91a3424
lennertjansen <lennertjansen95@gmail.com>
2021-03-01 17:02:02 +0100

    Started data precprocessing and exploratory analysis of blog authorship corpus

---
+++ data_preprocessing/dataset.py
@@ -0,0 +1,19 @@
+# Handles pre-processing of data and implementation of dataset type.
+#
+# Date created: 1 March 2021
+################################################################################
+
+# Import statements
+import pdb
+import os
+import numpy as np
+import torch.utils.data as data
+
+class TextDataset(data.Dataset):
+
+    '''
+    Text dataset type. Inherits functionality from torch.utils.data.Dataset.
+    '''
+
+    def __init__(self):
+        pass
