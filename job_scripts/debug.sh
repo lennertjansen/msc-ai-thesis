3043e72007850255759939131371456694064afb
lennertjansen <lennertjansen95@gmail.com>
2021-07-21 13:27:10 +0200

    shell script for debugging on Lisa correction

--- job_scripts/debug.sh
+++ job_scripts/debug.sh
@@ -4,6 +4,8 @@
 #SBATCH --nodes=1
 #SBATCH --job-name=debugger
 #SBATCH --time=1:00:00
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=lennertjansen95@gmail.com
 #SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/debugger.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
 
 # Loading all necessary modules.
