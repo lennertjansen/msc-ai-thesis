4bc43d322b10a3d5538c539fb994c29800850870
lennertjansen <lennertjansen95@gmail.com>
2021-07-23 15:32:05 +0200

    Adjusted debugger job script for Blog corpus on train_classifiers.py. Lets see what happens

--- job_scripts/debug.sh
+++ job_scripts/debug.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --job-name=debugger
-#SBATCH --time=1:00:00
+#SBATCH --time=3:00:00
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=lennertjansen95@gmail.com
 #SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/debugger.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
@@ -27,25 +27,49 @@ source activate /home/lennertj/miniconda3/envs/thesis_lisa2
 
 # Change directories
 echo "Changing directory"
-cd $HOME/code/PPLM
-#cd $HOME/code/msc-ai-thesis
+#cd $HOME/code/PPLM
+cd $HOME/code/msc-ai-thesis
 
 # Run your code
 echo "Running python code..."
 ## declare an array variable
-declare -a arr=("bert-base-uncased")
+#declare -a arr=("bert-base-uncased")
 
 ## now loop through the above array
-for i in "${arr[@]}"
-do
-  for j in 1 2
-  do
+#for i in "${arr[@]}"
+#do
+#  for j in 1 2 3
+#  do
+#
+#    python run_pplm_discrim_train.py --dataset 'generic' \
+#          --dataset_fp '/home/lennertj/code/msc-ai-thesis/data/bnc/bnc_rb_full_generic_pplm.txt' \
+#          --epochs 5 \
+#          --batch_size 64 \
+#          --log_interval 20000 \
+#          --pretrained_model "$i"
+#  done
+#done
 
-    python run_pplm_discrim_train.py --dataset 'generic' \
-          --dataset_fp '/home/lennertj/code/msc-ai-thesis/data/bnc/bnc_rb_full_generic_pplm.txt' \
-          --epochs 5 \
-          --batch_size 64 \
-          --log_interval 20000 \
-          --pretrained_model "$i"
-  done
-done
+for seed in 2021 2022
+do
+
+  python train_classifiers.py \
+         --data 'blog' \
+         --model_type 'lstm' \
+         --mode 'train' \
+         --seed "$seed" \
+         --batch_size 64 \
+         --embedding_dim 128 \
+         --hidden_dim 256 \
+         --num_layers 2 \
+         --bidirectional \
+         --batch_first \
+         --epochs 2 \
+         --lr 0.001 \
+         --es 3 \
+         --train_frac 0.75 \
+         --val_frac 0.15 \
+         --test_frac 0.1 \
+         --subset_size 10000 \
+         --log_interval 100 \
+         --no_tb \
