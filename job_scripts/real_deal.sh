797a4b64c67e984bec82a88be33b385104164984
lennertjansen <lennertjansen95@gmail.com>
2021-07-24 17:16:40 +0200

    Real deal job script for blog x BERT (finetuned, own implementation).

--- job_scripts/real_deal.sh
+++ job_scripts/real_deal.sh
@@ -4,11 +4,11 @@
 #SBATCH -p gpu_titanrtx_shared ## Select the partition. This one is almost always free, and has TitanRTXes (much RAM)
 #SBATCH --nodes=1
 ##SBATCH --gpus-per-node=1
-#SBATCH --job-name=real_deal_blog_lstm
+#SBATCH --job-name=real_deal_blog_bert_finetune
 #SBATCH --time=24:00:00
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=lennertjansen95@gmail.com
-#SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/real_deal_blog_lstm.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
+#SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/real_deal_blog_bert_finetune.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
 
 # Loading all necessary modules.
 echo "Loading modules..."
@@ -58,10 +58,10 @@ do
 
   python train_classifiers.py \
          --data 'blog' \
-         --model_type 'lstm' \
+         --model_type 'bert' \
          --mode 'train' \
          --seed "$seed" \
-         --batch_size 64 \
+         --batch_size 8 \
          --embedding_dim 128 \
          --hidden_dim 256 \
          --num_layers 2 \
