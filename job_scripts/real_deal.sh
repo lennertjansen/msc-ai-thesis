dc7509cfc3294aba683ef8325805849636c0559c
lennertjansen <lennertjansen95@gmail.com>
2021-07-26 11:05:42 +0200

    job script for blog x bert on 100k subset with batch size 4.:

--- job_scripts/real_deal.sh
+++ job_scripts/real_deal.sh
@@ -4,11 +4,11 @@
 #SBATCH -p gpu_titanrtx_shared ## Select the partition. This one is almost always free, and has TitanRTXes (much RAM)
 #SBATCH --nodes=1
 ##SBATCH --gpus-per-node=1
-#SBATCH --job-name=real_deal_blog_gpt2_fr_pplm
+#SBATCH --job-name=real_deal_blog_bert_finetune_subset_100k_bs4
 #SBATCH --time=24:00:00
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=lennertjansen95@gmail.com
-#SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/real_deal_blog_gpt2_fr_pplm.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
+#SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/real_deal_blog_bert_finetune_subset_100k_bs4.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
 
 # Loading all necessary modules.
 echo "Loading modules..."
@@ -24,17 +24,17 @@ echo "Activating conda environment..."
 source /home/lennertj/miniconda3/etc/profile.d/conda.sh
 source activate base
 conda info --envs
-source activate /home/lennertj/miniconda3/envs/thesis_lisa2
+source activate /home/lennertj/miniconda3/envs/thesis_lisa
 
 # Change directories
 echo "Changing directory"
-cd $HOME/code/PPLM
-#cd $HOME/code/msc-ai-thesis
+#cd $HOME/code/PPLM
+cd $HOME/code/msc-ai-thesis
 
 # Run your code
 echo "Running python code..."
- declare an array variable
-declare -a arr=("gpt2-medium")
+# declare an array variable
+#declare -a arr=("gpt2-medium")
 #declare -a arr=("bert-base-uncased")
 
 # data fps
@@ -42,41 +42,42 @@ declare -a arr=("gpt2-medium")
 # --dataset_fp '/home/lennertj/code/msc-ai-thesis/data/blogs_kaggle/blog_generic_pplm.txt' \
 
 ## now loop through the above array
-for i in "${arr[@]}"
-do
-  for j in 1 2 3 4 5
-  do
-
-    python run_pplm_discrim_train.py --dataset 'generic' \
-          --dataset_fp '/home/lennertj/code/msc-ai-thesis/data/blogs_kaggle/blog_generic_pplm.txt' \
-          --epochs 15 \
-          --batch_size 16 \
-          --log_interval 20000 \
-          --pretrained_model "$i"
-  done
-done
-
-#for seed in 2021 2022 2023 2024 2025
+#for i in "${arr[@]}"
 #do
-#  echo 'Starting new seed:'
-#  echo "$seed"
+#  for j in 1 2 3 4 5
+#  do
 #
-#  python train_classifiers.py \
-#         --data 'blog' \
-#         --model_type 'bert' \
-#         --mode 'train' \
-#         --seed "$seed" \
-#         --batch_size 2 \
-#         --embedding_dim 128 \
-#         --hidden_dim 256 \
-#         --num_layers 2 \
-#         --batch_first \
-#         --epochs 10 \
-#         --lr 0.001 \
-#         --early_stopping_patience 3 \
-#         --train_frac 0.75 \
-#         --val_frac 0.15 \
-#         --test_frac 0.1 \
-#         --log_interval 10000 \
-#         --no_tb
+#    python run_pplm_discrim_train.py --dataset 'generic' \
+#          --dataset_fp '/home/lennertj/code/msc-ai-thesis/data/blogs_kaggle/blog_generic_pplm.txt' \
+#          --epochs 15 \
+#          --batch_size 16 \
+#          --log_interval 20000 \
+#          --pretrained_model "$i"
+#  done
 #done
+
+for seed in 1 2 3 4
+do
+  echo 'Starting new seed:'
+  echo "$seed"
+
+  python train_classifiers.py \
+         --data 'blog' \
+         --model_type 'bert' \
+         --mode 'train' \
+         --seed "$seed" \
+         --batch_size 4 \
+         --embedding_dim 128 \
+         --hidden_dim 256 \
+         --num_layers 2 \
+         --batch_first \
+         --epochs 5 \
+         --lr 0.001 \
+         --early_stopping_patience 3 \
+         --train_frac 0.75 \
+         --val_frac 0.15 \
+         --test_frac 0.1 \
+         --log_interval 10000 \
+         --subset_size 100000 \
+         --no_tb
+done
