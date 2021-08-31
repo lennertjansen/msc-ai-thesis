d1177338087039599d815dfa7fdcd3756ccdb509
lennertjansen <lennertjansen95@gmail.com>
2021-08-31 15:51:22 +0200

    Removed unused import that leads to error on Lisa.

--- plug_play/run_pplm.py
+++ plug_play/run_pplm.py
@@ -34,7 +34,7 @@ from torch.autograd import Variable
 from tqdm import trange
 from transformers import GPT2Tokenizer
 from transformers.file_utils import cached_path
-from transformers.modeling_gpt2 import GPT2LMHeadModel
+# from transformers.modeling_gpt2 import GPT2LMHeadModel
 
 from pplm_classification_head import ClassificationHead
 
