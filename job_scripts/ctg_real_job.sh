a212574f2f964f40cd39da05d82f1fe5e2ef0e56
lennertjansen <lennertjansen95@gmail.com>
2021-09-02 15:15:50 +0200

    Job script for ctg bow young 50mi unigrams.

--- job_scripts/ctg_real_job.sh
+++ job_scripts/ctg_real_job.sh
@@ -4,11 +4,11 @@
 #SBATCH -p gpu_titanrtx_shared ## Select the partition. This one is almost always free, and has TitanRTXes (much RAM)
 #SBATCH --nodes=1
 ##SBATCH --gpus-per-node=1
-#SBATCH --job-name=ctg_old_bow_50miu
+#SBATCH --job-name=ctg_young_bow_50miu
 #SBATCH --time=5-00:00:00 ## Max time your script runs for (max is 5-00:00:00 | 5 days)
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=lennertjansen95@gmail.com
-#SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/ctg_old_bow_50miu.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
+#SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/ctg_young_bow_50miu.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
 
 # Loading all necessary modules.
 echo "Loading modules..."
@@ -42,7 +42,7 @@ do
            --pretrained_model 'gpt2-medium' \
            --cond_text 'My first impression' \
            --num_samples 30 \
-           --bag_of_words 'plug_play/wordlists/bnc_rb_50_mi_unigrams_old.txt' \
+           --bag_of_words 'plug_play/wordlists/bnc_rb_50_mi_unigrams_young.txt' \
            --length $length \
            --seed $seed \
            --sample \
