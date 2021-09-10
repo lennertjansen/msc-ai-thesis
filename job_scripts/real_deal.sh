6587afcd198555eef123b41cccca2d2867422d59
lennertjansen <lennertjansen95@gmail.com>
2021-09-10 16:51:33 +0200

    bnc-rb incl stopwords 2nd best LSTM.

--- job_scripts/real_deal.sh
+++ job_scripts/real_deal.sh
@@ -4,11 +4,11 @@
 #SBATCH -p gpu_titanrtx_shared ## Select the partition. This one is almost always free, and has TitanRTXes (much RAM)
 #SBATCH --nodes=1
 ##SBATCH --gpus-per-node=1
-#SBATCH --job-name=real_deal_bnc_ws_bert_frozen_pplmcode_alt_seeds
+#SBATCH --job-name=real_deal_bnc_ws_2ndbestlstm
 #SBATCH --time=5-00:00:00 ## Max time your script runs for (max is 5-00:00:00 | 5 days)
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=lennertjansen95@gmail.com
-#SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/real_deal_bnc_ws_bert_frozen_pplmcode_alt_seeds.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
+#SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/real_deal_bnc_ws_2ndbestlstm.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
 
 # Loading all necessary modules.
 echo "Loading modules..."
@@ -24,65 +24,64 @@ echo "Activating conda environment..."
 source /home/lennertj/miniconda3/etc/profile.d/conda.sh
 source activate base
 conda info --envs
-source activate /home/lennertj/miniconda3/envs/thesis_lisa2
+source activate /home/lennertj/miniconda3/envs/thesis_lisa
 
 # Change directories
 echo "Changing directory"
-cd $HOME/code/PPLM
-#cd $HOME/code/msc-ai-thesis
+#cd $HOME/code/PPLM
+cd $HOME/code/msc-ai-thesis
 
 # Run your code
 echo "Running python code..."
 # declare an array variable
 #declare -a arr=("gpt2-medium")
-declare -a arr=("bert-base-uncased")
+#declare -a arr=("bert-base-uncased")
 
 # data fps
 # --dataset_fp '/home/lennertj/code/msc-ai-thesis/data/bnc/bnc_rb_full_generic_pplm.txt' \
 # --dataset_fp '/home/lennertj/code/msc-ai-thesis/data/blogs_kaggle/blog_generic_pplm.txt' \
 
- now loop through the above array
-for i in "${arr[@]}"
-do
-  for j in 1 2 3 4 5
-  do
-
-    python run_pplm_discrim_train.py --dataset 'generic' \
-          --dataset_fp '../msc-ai-thesis/data/bnc/bnc_rb_incl_stopwords_nonalph_generic_pplm.txt' \
-          --epochs 10 \
-          --batch_size 16 \
-          --log_interval 20000 \
-          --pretrained_model "$i"
-  done
-done
-
-#for seed in 1 2 3 4 5 6 7 8 9 10
+#now loop through the above array
+#for i in "${arr[@]}"
 #do
-#  echo 'Starting new seed:'
-#  echo "$seed"
+#  for j in 1 2 3 4 5
+#  do
 #
-#  python train_classifiers.py \
-#         --data 'bnc_rb' \
-#         --model_type 'lstm' \
-#         --mode 'train' \
-#         --seed "$seed" \
-#         --batch_size 64 \
-#         --embedding_dim 64 \
-#         --hidden_dim 128 \
-#         --num_layers 1 \
-#         --bidirectional \
-#         --batch_first \
-#         --epochs 10 \
-#         --lr 0.001 \
-#         --early_stopping_patience 3 \
-#         --train_frac 0.75 \
-#         --val_frac 0.15 \
-#         --test_frac 0.1 \
-#         --log_interval 1000 \
-#         --no_tb \
-#         --w_sampling \
-#         --w_loss \
-#         --dropout 0.1
+#    python run_pplm_discrim_train.py --dataset 'generic' \
+#          --dataset_fp '../msc-ai-thesis/data/bnc/bnc_rb_incl_stopwords_nonalph_generic_pplm.txt' \
+#          --epochs 10 \
+#          --batch_size 16 \
+#          --log_interval 20000 \
+#          --pretrained_model "$i"
+#  done
 #done
+
+for seed in 1 2 3 4 5 6 7 8 9 10
+do
+  echo 'Starting new seed:'
+  echo "$seed"
+
+  python train_classifiers.py \
+         --data 'bnc_rb' \
+         --model_type 'lstm' \
+         --mode 'train' \
+         --seed "$seed" \
+         --batch_size 64 \
+         --embedding_dim 512 \
+         --hidden_dim 1024 \
+         --num_layers 2 \
+         --batch_first \
+         --epochs 10 \
+         --lr 0.0001 \
+         --early_stopping_patience 3 \
+         --train_frac 0.75 \
+         --val_frac 0.15 \
+         --test_frac 0.1 \
+         --log_interval 1000 \
+         --no_tb \
+         --w_sampling \
+         --w_loss \
+         --dropout 0.1
+done
 
 #python case_analysis.py
