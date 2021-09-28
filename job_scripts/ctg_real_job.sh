2b9638b5af7c931dec862755c130ab3bc8521d72
lennertjansen <lennertjansen95@gmail.com>
2021-09-28 08:41:18 +0200

    proper job script for GPT2-medium, proper questions, OLD, quick sample generation.

--- job_scripts/ctg_real_job.sh
+++ job_scripts/ctg_real_job.sh
@@ -4,11 +4,11 @@
 #SBATCH -p gpu_titanrtx_shared ## Select the partition. This one is almost always free, and has TitanRTXes (much RAM)
 #SBATCH --nodes=1
 ##SBATCH --gpus-per-node=1
-#SBATCH --job-name=ctg_discrim_gpt2_ml512_lr_00001_WS_quick_sandro_young_proper_QA
+#SBATCH --job-name=ctg_discrim_gpt2_ml512_lr_00001_WS_quick_sandro_old_proper_QA
 #SBATCH --time=5-00:00:00 ## Max time your script runs for (max is 5-00:00:00 | 5 days)
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=lennertjansen95@gmail.com
-#SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/ctg_discrim_gpt2_ml512_lr_00001_WS_quick_sandro_young_proper_QA.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
+#SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/ctg_discrim_gpt2_ml512_lr_00001_WS_quick_sandro_old_proper_QA.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
 
 # Loading all necessary modules.
 echo "Loading modules..."
@@ -70,7 +70,7 @@ do
              --length $length \
              --seed $seed \
              --sample \
-             --class_label 0 \
+             --class_label 1 \
              --verbosity "quiet" \
              --discrim_weights "plug_play/discriminators/gpt2_incl_sw_nac/generic_pm_gpt2-medium_ml_512_lr_0.0001_classifier_head_epoch_19.pt" \
              --discrim_meta "plug_play/discriminators/gpt2_incl_sw_nac/generic_pm_gpt2-medium_ml_512_lr_0.0001_classifier_head_meta.json"
