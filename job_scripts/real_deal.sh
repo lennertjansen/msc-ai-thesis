6aa1dbc64921036e421b4899b4481c7184200196
lennertjansen <lennertjansen95@gmail.com>
2021-10-06 08:26:28 +0200

    ACTUALLY BLOGS WS NOW. BERT-ft

--- job_scripts/real_deal.sh
+++ job_scripts/real_deal.sh
@@ -4,11 +4,11 @@
 #SBATCH -p gpu_titanrtx_shared ## Select the partition. This one is almost always free, and has TitanRTXes (much RAM)
 #SBATCH --nodes=1
 ##SBATCH --gpus-per-node=1
-#SBATCH --job-name=classif_blog_ws_bert_ft_7seeds
+#SBATCH --job-name=classif_blog_ws_bert_ft_7seeds_ACTUALLY_BLOGS
 #SBATCH --time=5-00:00:00 ## Max time your script runs for (max is 5-00:00:00 | 5 days)
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=lennertjansen95@gmail.com
-#SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/classif_blog_ws_bert_ft_7seeds.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
+#SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/classif_blog_ws_bert_ft_7seeds_ACTUALLY_BLOGS.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
 
 # Loading all necessary modules.
 echo "Loading modules..."
@@ -62,7 +62,7 @@ do
   echo "$seed"
 
   python train_classifiers.py \
-         --data 'bnc_rb' \
+         --data 'blog' \
          --model_type 'bert' \
          --mode 'train' \
          --seed "$seed" \
