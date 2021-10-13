772c7d12f3a26c6cc38033d942f1f81437103b7a
lennertjansen <lennertjansen95@gmail.com>
2021-10-13 16:05:16 +0200

    Job script for more seeds Blogs WS BERT-ft. Seeds starting from 2023

--- job_scripts/real_deal.sh
+++ job_scripts/real_deal.sh
@@ -4,11 +4,11 @@
 #SBATCH -p gpu_titanrtx_shared ## Select the partition. This one is almost always free, and has TitanRTXes (much RAM)
 #SBATCH --nodes=1
 ##SBATCH --gpus-per-node=1
-#SBATCH --job-name=classif_ACTUALLY_BLOGS_ws_ACTUALLY_LSTM_hps_redo_100K_subset
+#SBATCH --job-name=classif_blog_ws_bert_ft_7seeds_ACTUALLY_BLOGS_more_seeds
 #SBATCH --time=5-00:00:00 ## Max time your script runs for (max is 5-00:00:00 | 5 days)
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=lennertjansen95@gmail.com
-#SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/classif_ACTUALLY_BLOGS_ws_ACTUALLY_LSTM_hps_redo_100K_subset.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
+#SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/classif_blog_ws_bert_ft_7seeds_ACTUALLY_BLOGS_more_seeds.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
 
 # Loading all necessary modules.
 echo "Loading modules..."
@@ -56,17 +56,47 @@ echo "Running python code..."
 #  done
 #done
 
-for seed in 2021
+#for seed in 2021
+#do
+#  echo 'Starting new seed:'
+#  echo "$seed"
+#
+#  python train_classifiers.py \
+#         --data 'blog' \
+#         --model_type 'lstm' \
+#         --mode 'val' \
+#         --seed "$seed" \
+#         --batch_size 4 \
+#         --embedding_dim 512 \
+#         --hidden_dim 1024 \
+#         --num_layers 2 \
+#         --batch_first \
+#         --epochs 10 \
+#         --lr 0.0001 \
+#         --early_stopping_patience 3 \
+#         --train_frac 0.75 \
+#         --val_frac 0.15 \
+#         --test_frac 0.1 \
+#         --log_interval 1000 \
+#         --no_tb \
+#         --w_sampling \
+#         --w_loss \
+#         --subset_size 100000 \
+#         --dropout 0.1
+#done
+
+
+for seed in 2023 2024 2025 2026 2027
 do
   echo 'Starting new seed:'
   echo "$seed"
 
   python train_classifiers.py \
          --data 'blog' \
-         --model_type 'lstm' \
-         --mode 'val' \
+         --model_type 'bert' \
+         --mode 'train' \
          --seed "$seed" \
-         --batch_size 4 \
+         --batch_size 8 \
          --embedding_dim 512 \
          --hidden_dim 1024 \
          --num_layers 2 \
@@ -81,7 +111,6 @@ do
          --no_tb \
          --w_sampling \
          --w_loss \
-         --subset_size 100000 \
          --dropout 0.1
 done
 
