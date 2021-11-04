a4637a86943a5d869f7b3cc38e2d2fbabbeec0a4
lennertjansen <lennertjansen95@gmail.com>
2021-11-04 18:06:48 +0100

    Utterance classification of YOUNG turnwise HOBBY scripted dialogues. ACTUALLY UTTERANCE CLASSIFICATION NOW. JEEEEZZ

--- job_scripts/ctg_real_job.sh
+++ job_scripts/ctg_real_job.sh
@@ -4,11 +4,11 @@
 #SBATCH -p gpu_titanrtx_shared ## Select the partition. This one is almost always free, and has TitanRTXes (much RAM)
 #SBATCH --nodes=1
 ##SBATCH --gpus-per-node=1
-#SBATCH --job-name=scripted_dialogue_generation_hobby_turnwise_YOUNG_RETRY
+#SBATCH --job-name=scripted_dialogue_generation_hobby_turnwise_YOUNG_utterance_classification
 #SBATCH --time=5-00:00:00 ## Max time your script runs for (max is 5-00:00:00 | 5 days)
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=lennertjansen95@gmail.com
-#SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/scripted_dialogue_generation_hobby_turnwise_YOUNG_RETRY.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
+#SBATCH -o /home/lennertj/code/msc-ai-thesis/SLURM/output/scripted_dialogue_generation_hobby_turnwise_YOUNG_utterance_classification.%A.out ## this is where the terminal output is printed to. %j is root job number, %a array number. try %j_%a ipv %A (job id)
 
 # Loading all necessary modules.
 echo "Loading modules..."
@@ -46,27 +46,27 @@ echo "Running python code..."
 declare -a young_prompts=("Tell me about your hobbies. Recently I have been riding horses a lot. It es great in summer as you get to spend time in nature." "I understand. Being outdoors is a great escape from a busy lifestyle." "So you no longer have a full-time job? Yes, I can imagine. Especially if your family lives far away." "What do you mean?" "That sounds lovely. I'm not so much of a hiker myself, but nice to hear." "Tell me about your hobbies. I love too cook. I also love to play football, but my knees are not in the best shape. So, us talking about food has been really nice!" "I grew up in Egypt for the first half of my life, and then I moved to Budapest with my brother and mother. You?" "Oh nice. I am in Amsterdam currently. I have been playing football since I was 8 years old. I love playing in the midfield... do you play any sports?" "Nice! Holland has some of the best footballers. What position did you play?" "Nice! My cousin was a centre-back. He was the best I'd ever seen. What other hobbies do you have?" "Tell me about your hobbies. My main hobbies are listening to music, watching movies and also dancing. What about you?" "What is your favorite genre in music?" "I am not a fan of metal." "My favorite song is 'Saturday Night' by Khalid." "My favorite movie as a child was 'Home Alone'. What about you?")
 #declare -a old_prompts=("Tell me about your hobbies. I have many hobbies. My oldest hobby is socializing and interacting with people. That's for me a hobby, not something that you just do." "Well, my hobby is helping people in a more fundamental way. By adjusting their nutritional needs and help them with specific issues such as fertility." "Sharing knowledge is fundamental to life and has a positive effect for the recipient as well for the given." "Tell me about your hobbies. Well I don't get much time for hobbies, but when I do, especially on holiday I love to Scuba Dive, I love flying, I enjoy technology, I ought to buy a drone, to capture lots of aerial shots and I enjoy skiing, but last time I fell and broke my wrist which was painful." "What kind of games do you enjoy? I like card games, strategy games, which require interaction with people. I did used to play computer games, but prefer to see the people I play against." "I like Chess, because it's a strategy game and requires thinking in advance of your opponents moves." "Hobbies help me relax from day to day work, but again finding the time for any personal hobbies is difficult, so playing game of chess can be equally rewarding, but unfortunately my son now beats me most of the time, I taught him too well." "Tell me about your hobbies. I enjoy badminton and chess which I play. I enjoy watching football also." "Good, what position do you play." "Do you score a lot of goals.Do you help out in defence as well." "Im too old to play for a team now. Do you have a favorite player.")
 
-# for discrim-based
-for prompt in "${young_prompts[@]}"
-do
-  echo "NEW PROMPT ALERT WOOOWOOOOWOOOOOOWOOOOOOO"
-  for seed in 2021
-  do
-    for length in 45
-    do
-      python plug_play/run_pplm.py \
-             --pretrained_model 'gpt2-medium' \
-             --num_samples 3 \
-             --bag_of_words "plug_play/wordlists/bnc_young_mcwu_ws_pct_85.txt" \
-             --length $length \
-             --seed $seed \
-             --sample \
-             --class_label 0 \
-             --verbosity "quiet" \
-             --cond_text "$prompt"
-    done
-  done
-done
+## for discrim-based
+#for prompt in "${young_prompts[@]}"
+#do
+#  echo "NEW PROMPT ALERT WOOOWOOOOWOOOOOOWOOOOOOO"
+#  for seed in 2021
+#  do
+#    for length in 45
+#    do
+#      python plug_play/run_pplm.py \
+#             --pretrained_model 'gpt2-medium' \
+#             --num_samples 3 \
+#             --bag_of_words "plug_play/wordlists/bnc_young_mcwu_ws_pct_85.txt" \
+#             --length $length \
+#             --seed $seed \
+#             --sample \
+#             --class_label 0 \
+#             --verbosity "quiet" \
+#             --cond_text "$prompt"
+#    done
+#  done
+#done
 
 #declare -a arr=("Tell me about your holidays. Sure! I went to Greece and had a very fun time." "Tell me about your favorite food. Of course. I love pasta!." "Have you seen the news lately? I can't believe what I saw.")
 #declare -a arr=("Hello, how are you?<|endoftext|>")
@@ -174,9 +174,13 @@ done
 #declare -a young_utts=("Tell me about your favourite food. I love sushi and Japanese food in general. What about you?" "I love sushi in general." "Anything salmon. I am not so much of a spicy food lover, however. I love food too." "So when I saw this amazing dish from one of my favorite foodie friends in town, I was excited to try it!" "Me too! Meat and fish all the way." "I love it!" "I agree! A lot of great fish and meat. And it is also nice and lean." "What I miss in a fish is some nice crunch to it." "Tell me about your favourite food. Too many to choose. I think one of my favorites for sure is a traditional stuffed pigeon from home. We eat pigeon a lot over there and it's usually stuffed with the most amazing herb rice! I usually eat it for special ocassions though, as it is quite hard to prepare and takes quite some time." "I also love making my own, as I love to try and make my own dishes that are unique and fun to eat." "Hmm.. Probably a traditional egyptian one too. It's called konafa." "This one is a bit more unusual." "Tell me about yours?" "My name is Chris, and I'm a freelance writer living in Toronto." "That's always a good idea! Me too. THe more local/fresh, the better. Tell me about your best dining experience!" "We were at Cinco de Mayo, and we were so impressed with the tacos at Tacolote that we had to go to the other side." "Tell me about your favourite food. My favorite food is fried chicken with French fries." "Fried chicken is my favourite food." "We share something in common." "We all feel that we are in a difficult spot, and we all want help." "I would describe myself as a fun-loving person. What about you?" "I am a fun-loving person and am happy with that." "What is your favorite drink?" "We love our drinks, but we love drinking all the time too." "I love apple juice." "Not just because it comes out clean, refreshing and tasty and has a good balance of sweetness from the apple.")
 #declare -a old_utts=("Tell me about your favourite food. Well, I am very fun with Mediterranean food, and especially food with herbs. Lot of fruits, nuts, especially in the summer weather." "I love all kinds of seafood, especially mussels." "I love seafood and fish, except I do not like chips! I much prefer olive fish accompanied with green salad." "I also like to add grilled salmon to my salad as it is quite healthy." "I actually I am not fond of cheese and it is not a food that I include in my diet" "I am not very fond of cheese and I do not like any dairy products that contain cheese in any way whatsoever." "It's not different at all. My favourite food is what I usually consume." "I love steak and chicken but sometimes I have to try it for something different because it tastes good, so sometimes I eat something different." "Tell me about your favourite food. Healthy or non healthy food, which would you prefer me to talk about?" "Healthy is the most difficult to talk about." "Well I love fish, all kinds of fish, I try and eat meat sparingly, love fruit and vegetables, so I ought to be very healthy, but unfortunately I have a sweet tooth for Chocolate." "I also like to drink milk, coffee and tea as it is quite healthy." "Well I love Cadburys, especially Fruit and Nut Chocolate, thinking the fruit and nut is somehow healthy for me. I also like Dark Chocolate as I know that should be better than milk chocolate, especially for the heart." "But I don't think I can eat a chocolate bar and not like milk chocolate, especially when it tastes of cream milk chocolate." "My allergies, well I thought that was typed as arteries, my arteries are probably clogged with chocolate, so not as healthy as I ought to be." "But I was thinking about it in a different light, and my blood work came back fine, but I still have some redness on my face and on my lips." "Salmon, which is fish, prawn, which is fish also, in terms of meat/animal, lamb, chicken and beef/steak. But as I mentioned earlier I try and eat more fruit and vegetables." "I also eat more whole milk, milk products, cheese and eggs." "Tell me about your favourite food. I enjoy spicy foods. I have a curry quite a lot." "But I don't really enjoy the sweet and sour food that we like in India, especially in the south of India." "I also enjoy fish and chips sometimes , but not too often as it is fattening." "I always like to add some fresh herbs to the fish for a nice change." "You need to have vegtables with your chips as it adds to the taste." "This is why I recommend to add them to a pan and heat it up." "Do you like halloumi? Its one of my favorites." "I love it because when I eat it its not greasy and it has some flavor that I love.")
 #
-## for discrim-based
-#for prompt in "${old_utts[@]}"
-#do
-#  echo "NEW PROMPT ABI"
-#  python plug_play/prompt_classifier.py --std --age_cat 1 --prompt "$prompt"
-#done
+
+# hobby topic utterances
+declare -a young_utts=("Tell me about your hobbies. Recently I have been riding horses a lot. It es great in summer as you get to spend time in nature." "My brother is a horse rider." "I understand. Being outdoors is a great escape from a busy lifestyle." "I'm always happy to have the sun and a warm place to sleep and warm up in the shade and sun." "So you no longer have a full-time job? Yes, I can imagine. Especially if your family lives far away." "I have been in a few different jobs over the years and my family's lives have gotten much more complicated than I thought they would." "What do you mean?" "I'm not talking about the people who say they have a problem with it, but the people who think you've changed your mind about it after seeing the videos or reading my posts on it." "That sounds lovely. I'm not so much of a hiker myself, but nice to hear." "I know you've been doing some hiking in the park, and it must be pretty hard work on those long backpacking days." "Tell me about your hobbies. I love too cook. I also love to play football, but my knees are not in the best shape. So, us talking about food has been really nice!" "I like to cook for myself and my friends as well." "I grew up in Egypt for the first half of my life, and then I moved to Budapest with my brother and mother. You?" "You have to remember that I grew up in the 1970s, when the Soviet Union was still the dominant superpower in this part of the world." "Oh nice. I am in Amsterdam currently. I have been playing football since I was 8 years old. I love playing in the midfield... do you play any sports?" "I have played in a few different sports but football is the one that I am most obsessed with." "Nice! Holland has some of the best footballers. What position did you play?" "I played centre forward for the Dutch National Team as a 19 year old, I had to work hard to get to where I am now." "Nice! My cousin was a centre-back. He was the best I'd ever seen. What other hobbies do you have?" "I do have quite a few other hobbies, but I've never been able to find time to do them." "Tell me about your hobbies. My main hobbies are listening to music, watching movies and also dancing. What about you?" "My most popular hobby is dancing!" "What is your favorite genre in music?" "I love everything from punk rock to hip-hop." "I am not a fan of metal." "My tastes don't include it." "My favorite song is 'Saturday Night' by Khalid." "This one is so fun and catchy with great lyrics." "My favorite movie as a child was 'Home Alone'. What about you?" "The new movie "Carnival of Monsters 2" is being directed by Steven Spielberg and stars the likes of Brad Pitt, Emma Stone, James Franco, and Kristen Stewart as the titular monsters.")
+
+# for discrim-based
+for prompt in "${young_utts[@]}"
+do
+  echo "NEW PROMPT ABI"
+  python plug_play/prompt_classifier.py --std --age_cat 0 --prompt "$prompt"
+done
